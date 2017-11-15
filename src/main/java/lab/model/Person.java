df213b6db4cb0f67ce18a997f70e3d38dda8c789
Жанна <Janny.Gorelova@gmail.com>
2017-11-15 17:48:49 +0300

    idea folder has been deleted from repository

--- src/main/java/lab/model/Person.java
+++ src/main/java/lab/model/Person.java
@@ -6,6 +6,6 @@ public interface Person {
     String getName();
 
     default void sayHello(Person person) {
-        System.out.printf("Hello, %s, I'm %s%n", person.getName());
+        System.out.printf("Hello, %s,  I'm %s%n", person.getName());
     }
 }
