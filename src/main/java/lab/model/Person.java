80e7004c43253b2d4e5658dc1aa3caa6be574247
Жанна <Janny.Gorelova@gmail.com>
2017-11-15 17:46:47 +0300

    Lab-3 has been done

--- src/main/java/lab/model/Person.java
+++ src/main/java/lab/model/Person.java
@@ -2,9 +2,10 @@ package lab.model;
 
 public interface Person {
     Person setName(String name);
-    String getName ();
+
+    String getName();
 
     default void sayHello(Person person) {
-        System.out.printf("Hello, %s%n", person.getName());
+        System.out.printf("Hello, %s, I'm %s%n", person.getName());
     }
 }
